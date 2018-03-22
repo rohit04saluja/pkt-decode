0caf7264ece3f6db474584f161f5efec07e3377b
Rohit Saluja <rohit04saluja@protonmail.com>
2018-03-22 13:04:34 +0530

    Adding gtest_main file for execution of all gtests

---
+++ src/gtest_main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ * Created by Rohit Saluja on 3/22/18.
+ * Copyright © 2018 rohit04saluja. All rights reserved.
+ */
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+GTEST_API_ int main (int argc, char *argv[]) {
+    cout << "Running main() from gtest_main.cc" << endl;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
