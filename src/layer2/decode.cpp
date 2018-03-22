5610e0f1e56d70941c82d6485c41ff7ee897be17
Rohit Saluja <rohit04saluja@protonmail.com>
2018-03-23 02:23:03 +0530

    Adding decoder for layer2 decoding
    Supports Ethernet packet only till now

--- src/layer2/decode.cpp
+++ src/layer2/decode.cpp
@@ -5,14 +5,24 @@
  * Copyright © 2018 rohit04saluja. All rights reserved.
  */
 
+#include "decode.hpp"
 #include "ethernet.hpp"
 
 using namespace std;
 
-int layer2_decode (uint8_t * pkt) {
+/*
+ * @brief
+ * API to decode layer2 of a packet
+ *
+ * @param[in]
+ * pkt      pointer to the start of the packet
+ *
+ * @return
+ * size of the layer2 header
+ */
+size_t layer2_decode (const uint8_t * pkt) {
     Ethernet eth(pkt);
 
     eth.print();
-    pkt += EthernetLen;
     return EthernetLen;
 }
---
+++ src/layer2/decode.hpp
@@ -0,0 +1,16 @@
+/*
+ * decode.hpp
+ *
+ * Created by Rohit Saluja on 3/23/18.
+ * Copyright © 2018 rohit04saluja. All rights reserved.
+ */
+
+#ifndef DECODE_H_
+#define DECODE_H_
+
+#include <cstdint>
+#include <string>
+
+size_t layer2_decode (const uint8_t * pkt);
+
+#endif  // DECODE_H_
