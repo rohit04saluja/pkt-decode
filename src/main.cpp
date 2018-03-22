6d114030b8c8d8f698f9a4c2cd92cf3374fe68cd
Rohit Saluja <rohit04saluja@protonmail.com>
2018-03-22 17:01:13 +0530

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * main.cpp
+ *
+ * Created by Rohit Saluja on 3/22/18.
+ * Copyright © 2018 rohit04saluja. All rights reserved.
+ */
+
+#include <iostream>
+#include "ethernet.hpp"
+
+int main (int argc, char *argv[]) {
+    return 0;
+}
