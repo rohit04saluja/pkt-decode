a359c6c28b51935cd7bd7063d381bec2c0c7ba1b
Rohit Saluja <rohit04saluja@protonmail.com>
2018-03-22 14:03:19 +0530

    Adding gtest for MacAddr class and member methods

--- src/layer2/unit-tests/ethernet_gtest.cpp
+++ src/layer2/unit-tests/ethernet_gtest.cpp
@@ -7,3 +7,32 @@
 
 #include <gtest/gtest.h>
 #include <ethernet.hpp>
+
+using namespace std;
+
+/*
+ * @brief
+ * Test to check the working of default constructor
+ */
+TEST (MacAddr, MacAddrDefault) {
+    MacAddr addr;
+    const uint8_t * out = addr.getAddr();
+
+    for (int i=0; i<MacAddrLen; i++) {
+        EXPECT_EQ(0, out[i]);
+    }
+
+    ostringstream os;
+    os << addr;
+    EXPECT_STREQ("00:00:00:00:00:00", os.str().c_str());
+}
+
+TEST (MacAddr, MacAddrInit) {
+    uint8_t pkt [MacAddrLen] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
+    MacAddr addr(pkt);
+    const uint8_t * out = addr.getAddr();
+
+    for (int i=0; i<MacAddrLen; i++) {
+        EXPECT_EQ(pkt[i], out[i]);
+    }
+}
